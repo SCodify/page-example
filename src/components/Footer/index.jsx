7766259de36e3236133b29234a0e27378e249642
Christian Ledesma <softcodify@gmail.com>
2023-11-17 02:07:11 -0300

    Initial commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+//import './styles.css'
+
+function index() {
+  return (
+    <>
+      <footer className="container-fuid bg-body-tertiary text-center py-5">
+        <small>SoftCodify - 2023</small>
+      </footer>
+    </>
+  )
+}
+
+export default index
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import './styles.css'
+
+import Jumbotron from '../Jumbotron'
+
+function index() {
+  return (
+    <>
+      <main className="container py-4 d-flex justify-content-center align-items-center" data-bs-theme="dark">
+        
+        <Jumbotron/>
+
+      </main>
+    </>
+  )
+}
+
+export default index
